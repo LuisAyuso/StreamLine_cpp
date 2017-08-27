40838b1d4f57a0f51b74721c203d2d3a336296a9
LuisAyuso <luis.f.ayuso@gmail.com>
2017-08-27 17:48:44 +0200

    experimental cpp strucuture

--- code/main.cpp
+++ code/main.cpp
@@ -1,10 +1,31 @@
 #include <iostream>
 
 #include "streamline.h"
+
 #include <glbinding/gl/gl.h>
 
-int main() {
-    std::cout << "hello!" << std::endl;
+#include <nodes/ConnectionStyle>
+#include <nodes/FlowScene>
+#include <nodes/FlowView>
+#include <nodes/NodeData>
+
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QMenuBar>
+#include <QtWidgets/QVBoxLayout>
+
+#include <nodes/DataModelRegistry>
+
+int main(int argc, char* argv[]) {
+    QApplication app(argc, argv);
+
+    QWidget mainWidget;
+
+    std::cout << "hello, with dependencies!" << std::endl;
     streamline::function(1);
-    return 0;
+
+    mainWidget.setWindowTitle("We are puting out there a window!!!");
+    mainWidget.resize(800, 600);
+    mainWidget.showNormal();
+
+    return app.exec();
 }
