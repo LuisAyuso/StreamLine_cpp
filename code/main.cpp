a5920343b96ab71e55ce8d22704325865be4d097
LuisAyuso <luis.f.ayuso@gmail.com>
2017-08-29 22:49:40 +0200

    first build, new dependency for text editor

--- code/main.cpp
+++ code/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 
+#include "nodes/image_show_model.hpp"
 #include "streamline.h"
 
 #include <glbinding/gl/gl.h>
@@ -15,17 +16,25 @@
 
 #include <nodes/DataModelRegistry>
 
+using QtNodes::DataModelRegistry;
+using QtNodes::FlowScene;
+using QtNodes::FlowView;
+
+static std::shared_ptr<DataModelRegistry> registerDataModels() {
+    auto ret = std::make_shared<DataModelRegistry>();
+    ret->registerModel<ImageShowModel>();
+    return ret;
+}
+
 int main(int argc, char* argv[]) {
     QApplication app(argc, argv);
 
-    QWidget mainWidget;
+    FlowScene scene(registerDataModels());
+    FlowView view(&scene);
 
-    std::cout << "hello, with dependencies!" << std::endl;
-    streamline::function(1);
-
-    mainWidget.setWindowTitle("We are puting out there a window!!!");
-    mainWidget.resize(800, 600);
-    mainWidget.showNormal();
+    view.setWindowTitle("We are puting out there a window!!!");
+    view.resize(800, 600);
+    view.showNormal();
 
     return app.exec();
 }
